49249c6c165ccb7eae7622d8b81358980ecf4ec8
Johannes Alt <altjohannes510@gmail.com>
2025-06-02 19:02:35 +0200

    chore(workspace): restructure projects as NX workspace for better modularity

---
+++ libs/ngrx-forms/src/lib/array/reducer/util.ts
@@ -0,0 +1,99 @@
+import { Actions } from '../../actions';
+import { formStateReducer } from '../../reducer';
+import { computeArrayState, FormArrayState, FormGroupControls, FormGroupState, FormState, isArrayState, isGroupState, KeyValue } from '../../state';
+
+export function dispatchActionPerChild<TValue>(
+  controls: readonly FormState<TValue>[],
+  actionCreator: (controlId: string) => Actions<TValue>,
+): readonly FormState<TValue>[] {
+  let hasChanged = false;
+  const newControls = controls
+    .map(state => {
+      const newState = formStateReducer<TValue>(state, actionCreator(state.id));
+      hasChanged = hasChanged || state !== newState;
+      return newState;
+    });
+
+  return hasChanged ? newControls : controls;
+}
+
+function callChildReducers<TValue>(
+  controls: readonly FormState<TValue>[],
+  action: Actions<TValue[]>,
+): readonly FormState<TValue>[] {
+  let hasChanged = false;
+  const newControls = controls
+    .map(state => {
+      const newState = formStateReducer<TValue>(state, action);
+      hasChanged = hasChanged || state !== newState;
+      return newState;
+    });
+
+  return hasChanged ? newControls : controls;
+}
+
+export function childReducer<TValue>(state: FormArrayState<TValue>, action: Actions<TValue[]>) {
+  const controls = callChildReducers(state.controls, action);
+
+  if (state.controls === controls) {
+    return state;
+  }
+
+  return computeArrayState(
+    state.id,
+    controls,
+    state.value,
+    state.errors,
+    state.pendingValidations,
+    state.userDefinedProperties,
+    {
+      wasOrShouldBeDirty: state.isDirty,
+      wasOrShouldBeEnabled: state.isEnabled,
+      wasOrShouldBeTouched: state.isTouched,
+      wasOrShouldBeSubmitted: state.isSubmitted,
+    },
+  );
+}
+
+export function updateIdRecursiveForGroup<TValue extends KeyValue = any>(state: FormGroupState<TValue>, newId: string): FormGroupState<TValue> {
+  const controls: FormGroupControls<TValue> =
+    Object.keys(state.controls)
+      .reduce((agg, key) => Object.assign(agg, {
+        [key]: updateIdRecursive<TValue[keyof TValue]>(state.controls[key as keyof TValue], `${newId}.${key}`),
+      }), {} as FormGroupControls<TValue>);
+
+  return {
+    ...state,
+    id: newId,
+    controls,
+  };
+}
+
+export function updateIdRecursiveForArray<TValue>(state: FormArrayState<TValue>, newId: string): FormArrayState<TValue> {
+  const controls = state.controls.map((c, i) => updateIdRecursive(c, `${newId}.${i}`));
+
+  return {
+    ...state,
+    id: newId,
+    controls,
+  };
+}
+
+export function updateIdRecursive<TValue extends KeyValue = any>(state: FormState<TValue>, newId: string): FormState<TValue> {
+  if (state.id === newId) {
+    return state;
+  }
+
+  if (isGroupState<TValue>(state)) {
+    return updateIdRecursiveForGroup<TValue>(state, newId) as FormState<TValue>;
+  }
+
+  if (isArrayState<TValue>(state)) {
+    return updateIdRecursiveForArray<TValue>(state, newId) as any;
+  }
+
+  return {
+    ...(state as any),
+    id: newId,
+  };
+}
---
+++ libs/ngrx-forms/src/lib/group/local-state-directive.ts
@@ -0,0 +1,23 @@
+import { Directive, EventEmitter, Output } from '@angular/core';
+
+import { Actions } from '../actions';
+import { KeyValue } from '../state';
+import { NgrxFormDirective } from './directive';
+
+@Directive({
+    // tslint:disable-next-line:directive-selector
+    selector: 'form[ngrxFormState][ngrxFormsAction]',
+    standalone: false
+})
+export class NgrxLocalFormDirective<TStateValue extends KeyValue> extends NgrxFormDirective<TStateValue> {
+
+  @Output() ngrxFormsAction = new EventEmitter<Actions<TStateValue>>();
+
+  constructor() {
+    super(null);
+  }
+
+  protected override dispatchAction(action: Actions<TStateValue>) {
+    this.ngrxFormsAction.emit(action);
+  }
+}
