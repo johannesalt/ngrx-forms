a487442a7949aba4d22f374aba04dec91a6a28c9
johannesalt <altjohannes510@gmail.com>
2026-04-22 12:52:37 +0200

    refactor(ngrx-forms): use the form action dispatcher

--- libs/ngrx-forms/src/lib/group/directive.spec.ts
+++ libs/ngrx-forms/src/lib/group/directive.spec.ts
@@ -1,9 +1,6 @@
 import { Component, ElementRef, input, viewChild } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Action, Store } from '@ngrx/store';
-import { provideMockStore } from '@ngrx/store/testing';
-import { MockInstance } from 'vitest';
-import { MarkAsSubmittedAction } from '../actions';
+import { FormActionDispatcher, NGRX_FORM_ACTION_DISPATCHER } from '../dispatcher';
 import { createFormGroupState, FormGroupState } from '../state';
 import { NgrxFormDirective } from './directive';
 
@@ -24,7 +21,7 @@ export class TestComponent {
 
   public readonly state = input<FormGroupState<any>>(INITIAL_STATE);
 
-  public submitForm() {
+  public submit() {
     const button = this.button();
     if (!button || !button.nativeElement) {
       throw 'Button cannot be null';
@@ -38,10 +35,32 @@ describe(NgrxFormDirective, () => {
   let component: TestComponent;
   let fixture: ComponentFixture<TestComponent>;
 
+  let dispatcher: FormActionDispatcher;
+  beforeEach(() => {
+    dispatcher = {
+      focus: vi.fn(() => null),
+      markAsDirty: vi.fn(() => null),
+      markAsPristine: vi.fn(() => null),
+      markAsSubmitted: vi.fn(() => null),
+      markAsTouched: vi.fn(() => null),
+      markAsUntouched: vi.fn(() => null),
+      reset: vi.fn(() => null),
+      setValue: vi.fn(() => null),
+      unfocus: vi.fn(() => null),
+    };
+  });
+
+  beforeEach(() => {
+    TestBed.overrideDirective(NgrxFormDirective, {
+      set: {
+        providers: [{ provide: NGRX_FORM_ACTION_DISPATCHER, useValue: dispatcher }],
+      },
+    });
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TestComponent],
-      providers: [provideMockStore()],
     }).compileComponents();
   });
 
@@ -51,24 +70,18 @@ describe(NgrxFormDirective, () => {
     fixture.detectChanges();
   });
 
-  let dispatch: MockInstance<(action: Action) => void>;
-  beforeEach(() => {
-    const store = TestBed.inject(Store);
-    dispatch = vi.spyOn(store, 'dispatch');
+  it(`should call a markAsSubmitted if the form is submitted and the state is unsubmitted`, () => {
+    component.submit();
+
+    expect(dispatcher.markAsSubmitted).toHaveBeenCalledWith(INITIAL_STATE.id);
   });
 
-  it(`should dispatch a ${MarkAsSubmittedAction} if the form is submitted and the state is unsubmitted`, () => {
-    component.submitForm();
-
-    expect(dispatch).toHaveBeenCalledWith(new MarkAsSubmittedAction(INITIAL_STATE.id));
-  });
-
-  it(`should not dispatch a ${MarkAsSubmittedAction} if the form is submitted and the state is submitted`, () => {
+  it(`should not call a markAsSubmitted if the form is submitted and the state is submitted`, () => {
     fixture.componentRef.setInput('state', { ...INITIAL_STATE, isSubmitted: true, isUnsubmitted: false });
     fixture.detectChanges();
 
-    component.submitForm();
+    component.submit();
 
-    expect(dispatch).not.toHaveBeenCalledWith(new MarkAsSubmittedAction(INITIAL_STATE.id));
+    expect(dispatcher.markAsSubmitted).not.toHaveBeenCalledWith(INITIAL_STATE.id);
   });
 });
--- libs/ngrx-forms/src/lib/group/local-state-directive.ts
+++ libs/ngrx-forms/src/lib/group/local-state-directive.ts
@@ -1,16 +1,26 @@
 import { Directive, output } from '@angular/core';
-
 import { Actions } from '../actions';
+import { BaseFormActionDispatcher, NGRX_FORM_ACTION_DISPATCHER } from '../dispatcher';
 import { KeyValue } from '../state';
 import { NgrxFormDirective } from './directive';
 
 @Directive({
-  selector: 'form[ngrxFormState][ngrxFormsAction]',
+  selector: 'form[ngrxFormsAction]',
+  hostDirectives: [
+    {
+      directive: NgrxFormDirective,
+      inputs: ['ngrxFormState'],
+    },
+  ],
+  providers: [{ provide: NGRX_FORM_ACTION_DISPATCHER, useExisting: NgrxLocalFormDirective }],
 })
-export class NgrxLocalFormDirective<TStateValue extends KeyValue> extends NgrxFormDirective<TStateValue> {
+export class NgrxLocalFormDirective<TStateValue extends KeyValue> extends BaseFormActionDispatcher<TStateValue> {
   public readonly ngrxFormsAction = output<Actions<TStateValue>>();
 
-  protected override dispatchAction(action: Actions<TStateValue>) {
+  /**
+   * @inheritdoc
+   */
+  public override dispatch(action: Actions<TStateValue>): void {
     this.ngrxFormsAction.emit(action);
   }
 }
