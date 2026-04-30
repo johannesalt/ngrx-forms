ca3024bb377f2f239e3448dd03b1d21ca2931377
johannesalt <altjohannes510@gmail.com>
2026-04-30 14:35:03 +0200

    feat(ngrx-forms): add function to add form reducer, methods and state to the signal store

--- libs/ngrx-forms/signals/src/lib/index.ts
+++ libs/ngrx-forms/signals/src/lib/index.ts
@@ -1,6 +1,7 @@
 export { FormConfig, withName, withOnStateChange, withUpdate } from './config';
 export * from './dispatcher';
 export * from './events';
+export * from './feature';
 export * from './methods';
 export * from './reducer';
 export * from './state';
